f8052b1ab8035f17ce772a8011e4f578cc8e852a
Pormiiles <andrelima.santos2370@gmail.com>
2024-03-12 17:48:36 -0300

    Criptografando senha + criando ProfileEnum

--- src/main/java/com/todosimple/todosimple/services/UserService.java
+++ src/main/java/com/todosimple/todosimple/services/UserService.java
@@ -1,17 +1,24 @@
 package com.todosimple.todosimple.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.todosimple.todosimple.models.User;
+import com.todosimple.todosimple.models.enums.ProfileEnum;
 import com.todosimple.todosimple.repositories.UserRepository;
 import com.todosimple.todosimple.services.exceptions.DataBindingViolationException;
 import com.todosimple.todosimple.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
     private UserRepository userRepository;
 
@@ -24,6 +31,8 @@ public class UserService {
     @org.springframework.transaction.annotation.Transactional
     public User createUser(User obj) {
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
 
         return obj;
@@ -33,6 +42,7 @@ public class UserService {
     public User updateUser(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
 
         return this.userRepository.save(newObj);
     }
